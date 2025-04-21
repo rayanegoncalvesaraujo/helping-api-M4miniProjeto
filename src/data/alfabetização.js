5ad8fd8a27003bae760689e8f8b6696612cab1f6
Rayane Gonçalves Araújo <rayanegoncalves796@gmail.com>
2025-04-21 12:13:30 -0300

    Inserindo arquivos: fome.js

---
+++ src/data/alfabetização.js
@@ -0,0 +1,7 @@
+// dados simulando índice de alfabetização
+export default {
+    brasilAlfabetizacao: 93.4,
+    faixaEtaria: "15 anos ou mais",
+    fonte: "IBGE(PNAD contínua)",
+    obs: "Regiões Norte e Nordeste apresentam os menores índices."
+};
---
+++ src/data/fome.js
@@ -0,0 +1,7 @@
+// dados simulando insegurança alimentar
+export default {
+    pessoasFome: 33000000,
+    ano: 2024,
+    fonte: "Rede Passan",
+    obs: "Mais de 15% da população enfrenta insegurança alimentar grave."
+};
